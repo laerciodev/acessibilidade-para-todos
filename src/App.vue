a117c73c96304737eb11fda398b00d1528df5695
Laércio <jose.laercio.bezerra@gmail.com>
2022-09-30 18:22:43 -0300

    feat: create header component

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <Header></Header>
+</template>
+
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <Logo />
+  </header>
+</template>
+
+<script lang="ts" setup>
+import Logo from './Logo.vue';
+</script>
+
+<style>
+.header {
+  height: 80px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(9, 18, 56, 0.3);
+}
+</style>
